5d04c9d879cd6b09329c3bd1edf49eabb92722e2
hndrr <yozhik20@gmail.com>
2020-08-19 21:59:12 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ const App = (props) => {
   const [name, setName] = useState(props.name);
   const [price, setPrice] = useState(props.price);
 
+  //初期値でリセットできる
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
+
   return (
     <>
       <p>
@@ -12,11 +18,13 @@ const App = (props) => {
       </p>
       <button onClick={() => setPrice((prevPrice) => prevPrice + 1)}>+1</button>
       <button onClick={() => setPrice((prevPrice) => prevPrice - 1)}>-1</button>
-      <button onClick={() => setPrice(props.price)}>reset</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
 };
 
+//初期値
 App.defaultProps = {
   name: "",
   price: 1000,
