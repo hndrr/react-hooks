a80ee7e0c604abacf941d155fcf8f652a1c29eeb
hndrr <yozhik20@gmail.com>
2020-08-20 02:39:56 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = (props) => {
   //初期値を渡す
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
-  //初期値でリセットできる
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  useEffect(() => {
+    console.log("like componentDidMount or componentDidUpdate");
+  });
+
+  //第２引数に空の配列を渡す
+  useEffect(() => {
+    console.log("like componentDidMount");
+  }, []);
+
+  useEffect(() => {
+    console.log("this callback is for name only.");
+  }, [name]);
 
   return (
     <>
       <p>
-        現在の{name}は{price}円です。
+        現在の{name}は{state.price}円です。
       </p>
-      <button onClick={() => setPrice((prevPrice) => prevPrice + 1)}>+1</button>
-      <button onClick={() => setPrice((prevPrice) => prevPrice - 1)}>-1</button>
-      <button onClick={reset}>reset</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>reset</button>
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
