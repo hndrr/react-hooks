7095d218f114fcddd40d6c728e4eaf93399930ee
hndrr <yozhik20@gmail.com>
2020-08-21 06:21:54 +0900

    reducer event

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+//
+//action = {
+//  type:'CREATE_EVENT',
+//  title:'aaaa',
+//  body:'bbb'
+//}
+//
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const id = (state.length = 0 ? 1 : state[state.length - 1].id + 1);
+      return [...state, { id, title: action.title, body: action.body }];
+    case "DELETE_EVENT":
+      return state;
+    case "DELETE_ALL_EVENTS":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default events;
