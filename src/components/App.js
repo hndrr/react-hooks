3d375cca320e98cd0fc9586f524791033e9b177f
hndrr <yozhik20@gmail.com>
2020-08-21 06:49:47 +0900

    イベント作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,62 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import reducer from "../reducers";
 import { Container, Form, Table, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+    console.log({ title, body });
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+    setTitle("");
+    setBody("");
+    //action = {
+    //  type:'CREATE_EVENT',
+    //  title:'aaaa',
+    //  body:'bbb'
+    //}
+  };
+  console.log(state);
+
   return (
     <Container fluid>
       <h4>イベント作成フォーム</h4>
       <Form>
         <Form.Group>
           <Form.Label htmlFor="formEventTitle">タイトル</Form.Label>
-          <Form.Control id="formEventTitle" />
+          <Form.Control
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
         </Form.Group>
 
         <Form.Group>
           <Form.Label htmlFor="formEventBody">ボディ</Form.Label>
-          <Form.Control as="textarea" id="formEventBody" />
+          <Form.Control
+            as="textarea"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => {
+              setBody(e.target.value);
+            }}
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={addEvent}>
           イベント作成
         </Button>
-        <Button variant="danger" type="submit">
-          イベント削除
-        </Button>
+        <Button variant="danger">イベント削除</Button>
       </Form>
 
       <h4>イベント一覧</h4>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,7 +9,7 @@
 const events = (state = [], action) => {
   switch (action.type) {
     case "CREATE_EVENT":
-      const id = (state.length = 0 ? 1 : state[state.length - 1].id + 1);
+      const id = state.length === 0 ? 1 : state[state.length - 1].id + 1;
       return [...state, { id, title: action.title, body: action.body }];
     case "DELETE_EVENT":
       return state;
