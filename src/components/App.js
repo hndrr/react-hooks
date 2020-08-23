de7aa9cd89ce620207a745605951e447c0e0999e
hndrr <yozhik20@gmail.com>
2020-08-23 19:00:26 +0900

    useContrext

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,22 @@ import React, { useReducer } from "react";
 import reducer from "../reducers";
 import EventForm from "./EventForm";
 import Events from "./Events";
+import AppContext from "../contexts/AppContext";
 import { Container } from "react-bootstrap";
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <Container fluid>
-      <h4>イベント作成フォーム</h4>
-      <EventForm state={state} dispatch={dispatch} />
+    <AppContext.Provider value={"ssss"}>
+      <Container fluid>
+        <h4>イベント作成フォーム</h4>
+        <EventForm state={state} dispatch={dispatch} />
 
-      <h4>イベント一覧</h4>
-      <Events state={state} dispatch={dispatch} />
-    </Container>
+        <h4>イベント一覧</h4>
+        <Events state={state} dispatch={dispatch} />
+      </Container>
+    </AppContext.Provider>
   );
 };
 
--- src/components/Events.js
+++ src/components/Events.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Event from "./Event";
+import AppContext from "../contexts/AppContext";
 import { Table } from "react-bootstrap";
 
 const Events = ({ state, dispatch }) => {
+  const value = useContext(AppContext);
   return (
-    <Table hover>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Body</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {state.map((event, index) => (
-          <Event key={index} event={event} dispatch={dispatch} />
-        ))}
-      </tbody>
-    </Table>
+    <>
+      <span>{value}</span>
+      <Table hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event, index) => (
+            <Event key={index} event={event} dispatch={dispatch} />
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
